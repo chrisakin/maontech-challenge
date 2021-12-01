a8cbc563b820dc8a1e08ee648eae796c3dd6e395
Christopher Akintade <chrisfemide@gmail.com>
2021-12-02 00:24:02 +0100

    Question test

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const questionController = require('../controller/questions')
 const checkJWT = require('../middleware/check-jwt');
 
-router.post('/', checkJWT, (questionController.postQuestions));
+router.post('/',  (questionController.postQuestions));
 router.get('/', questionController.getQuestions);
 router.post('/answers/:id',checkJWT, (questionController.postAnswers));
 router.post('/upvote/:id', questionController.upvoteAnswers);
